5bacd8a62daeaac06860d30308b0b5c09aaa8ea5
Malcolm Burtenshaw <mjburtenshaw@gmail.com>
2023-02-15 17:23:41 -0500

    Whitespace changes.

--- scripts/increment-version/index.sh
+++ scripts/increment-version/index.sh
@@ -14,12 +14,9 @@ curdir=$(dirname $0)
 
 # Execute all the desired scripts for your program here.
 # Execute check_errors after each command.
-
 # TIP: you can capture the new_version of a modified file from any script.
-
 # $version_increment_type is an environment variable you can configure in your shell source config file.
 # Its default value should be "patch". Change it to "minor" or "major" when applicable.
-
 new_version=$("$curdir"/shields.sh -i $version_increment_type)
 check_errors
 
